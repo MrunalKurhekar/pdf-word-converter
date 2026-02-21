fe8bdaccdb78120175e5751a9fb751d5e5462184
MrunalKurhekar <mrunal.k@anxilla.com>
2026-02-21 16:40:29 +0530

    changed for uploads App Api files

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "https://pdf-converter-backend-48r9.onrender.com/",
-// });
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
